d97027e2a16bdcb6339520362db794e28d985851
Herculano-Quimuanga <herculanoquimuanga0@gmail.com>
2025-06-16 21:06:44 +0100

    95% the landing

--- src/features/landing/hero/Hero.jsx
+++ src/features/landing/hero/Hero.jsx
@@ -9,13 +9,13 @@ function Hero() {
           <div className="hero__content">
             <p className="hero__title" data-aos="fade-up" data-aos-duration="1500">Get the best <strong>experience</strong> when sending messages</p>
 
-            <div className="hero__actions">
-              <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">Lorem ipsum dolor sit amet consectetur adipisicing. Labore excepturi quia zoue.</p>
-              <button className="btn" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="750">
+            <div className="hero__actions" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+              <p >Lorem ipsum dolor sit amet consectetur adipisicing. Labore excepturi quia zoue.</p>
+              <button className="btn">
                 <img src="/icons/user-blue.svg" alt="" />
                 Log In
               </button>
-              <button className="btn_2" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1000">
+              <button className="btn_2">
                 <img src="/icons/user-plus.svg" alt="" />
                 Sign In
               </button>
