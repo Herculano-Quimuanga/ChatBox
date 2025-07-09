090e4b49705cffc784ca5fc45fa26f226fa7eef4
Herculano-Quimuanga <herculanoquimuanga0@gmail.com>
2025-07-09 12:26:24 +0100

    Correct endpoint

--- src/features/auth/login/Login.jsx
+++ src/features/auth/login/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/usuarios/google`, form);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/usuarios/login`, form);
 
       localStorage.setItem("usuario", JSON.stringify(res.data.user));
       goTo('/'); // redireciona após login
--- src/features/auth/sign/Sign.jsx
+++ src/features/auth/sign/Sign.jsx
@@ -20,7 +20,7 @@ function Sign() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/usuarios/google`, form);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/usuarios/register`, form);
       localStorage.setItem("usuario", JSON.stringify(res.data.user));
       goTo('/');
       window.location.reload();
